25ba239dc0604fac500ae1df7aebd705c2b6d10d
luigi <luigilamastra@gmail.com>
2023-04-23 05:34:46 +0200

    works with simple htmlstring

--- src/pages/api/handleImage.js
+++ src/pages/api/handleImage.js
@@ -1,6 +1,8 @@
 
 import Card from '@/components/Card';
+import { getTodayYesterdayTomorrow } from '@/libs/dates';
 import generateImage from '@/libs/generateImage';
+import { getJson } from '@/libs/getJson';
 import nodeHtmlToImage from 'node-html-to-image'
 import ReactDOMServer from 'react-dom/server';
 import { zodiacSigns } from '../../../assets/zodiacSigns';
@@ -8,12 +10,12 @@ import { zodiacSigns } from '../../../assets/zodiacSigns';
 
 export default async function handler(req, res) {
     const htmlString = ReactDOMServer.renderToString('<h1>Ciao Mondo!!!</h1>');
-
-const signs = zodiacSigns
+const {today} = getTodayYesterdayTomorrow()
+const signs = await getJson(today)
     
     try {
       for (let sign of signs) {
-       await generateImage(sign.name, sign.name).then(console.log('IMMAGINE GENERATA'))
+       await generateImage(sign.horoscope, sign.sign).then(console.log('IMMAGINE GENERATA'))
 
       }
 
