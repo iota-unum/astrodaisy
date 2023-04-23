541941d208c5172a8fbedb30b0a690384f2eb6c1
luigi <luigilamastra@gmail.com>
2023-04-23 14:03:34 +0200

    refactored tweetCard

---
+++ src/libs/tweetImage.js
@@ -0,0 +1,15 @@
+import { TwitterApi } from "twitter-api-v2";
+import path from "path";
+const client = new TwitterApi({
+  appKey: process.env.API_KEY,
+  appSecret: process.env.API_KEY_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessSecret: process.env.ACCESS_TOKEN_SECRET,
+});
+export async function tweetCard(sign) {
+  const imagePath = path.join(process.cwd(), "public", "cards", sign + ".png");
+  const mediaId = await client.v1.uploadMedia(imagePath);
+  const newTweet = await client.v1.tweet("Hello!", { media_ids: mediaId });
+
+  return newTweet;
+}
--- src/pages/api/tweet.js
+++ src/pages/api/tweet.js
@@ -21,26 +21,19 @@
 //     res.status(400).json({ error });
 //   }
 // }
-import { TwitterApi } from "twitter-api-v2";
+
+import { tweetCard } from '@/libs/tweetImage';
 import path from 'path';
-const imagePath = path.join(process.cwd(), 'public', 'cards', 'toro.png');
 
 export default async function handler(req, res) {
   try {
    
-    const client = new TwitterApi({
-      appKey: process.env.API_KEY,
-      appSecret: process.env.API_KEY_SECRET,
-      accessToken: process.env.ACCESS_TOKEN,
-      accessSecret: process.env.ACCESS_TOKEN_SECRET,
-    });
+   const tweet = await tweetCard('pesci')
 
-    const mediaId = await client.v1.uploadMedia(imagePath);
-    const newTweet = await client.v1.tweet('Hello!', { media_ids: mediaId });
-    // const tweet = await client.v1.tweet("rivediamo");
+    
 
-    console.log("TWEET SENT!!!");
-    res.status(200).json({ res: "tweet sent!", newTweet });
+    console.log("TWEET SENT!!!", tweet);
+    res.status(200).json({ res: "tweet sent!", tweet });
   } catch (error) {
     console.log(error);
     res.status(400).json({ error });
